4d9a5578638fcabfe84b63cae17c9386a5e68119
jani-ux <janithpasiduuniinfo@gmail.com>
2025-06-24 10:04:44 +0530

    Update Pattern-With-Numbers.java

--- Pattern-With-Numbers.java
+++ Pattern-With-Numbers.java
@@ -255,5 +255,18 @@ public class Pattern_num {
         }
             System.out.println(" ");
         }
+        System.out.println(" ");
+         //Pyramid
+        System.out.println("Pyramid");
+        for(int i=n; i>0; i--){
+            for(int j=n; j>i; j--){
+                System.out.print("  ");
+            }
+            for(int j=1; j<= 2*i -1 ; j++){
+            System.out.print(" " + i);
+        }
+            System.out.println(" ");
+        }
+    }
     }
 }
