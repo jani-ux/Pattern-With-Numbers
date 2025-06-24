0516be37ac58a1d6bb72146bd9c9df40404fedb6
jani-ux <janithpasiduuniinfo@gmail.com>
2025-06-24 09:25:23 +0530

    Create Pattern-With-Numbers.java

---
+++ Pattern-With-Numbers.java
@@ -0,0 +1,247 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pattern_num;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author janit
+ */
+public class Pattern_num {
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int n;
+        // Get number of lines
+        System.out.print("Number of Lines : ");
+        n = myObj.nextInt();
+        //Right Angular Triangle Align Left
+        System.out.println("Right Angular Triangle Align Left");
+        for(int i=1; i <= n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+        
+        //Right Angular Triangle Align Left-Up
+        System.out.println("Right Angular Triangle Align Left-Up");
+        for(int i=n; i >0; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+        
+        //Right Angular Triangle Align Right
+        System.out.println("Right Angular Triangle Align Right");
+        for(int i=n; i >0; i--){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print("  ");
+                }else
+                {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+        
+        //Right Angular Triangle Align Up-Right
+        System.out.println("Right Angular Triangle Align Up-Right");
+        for(int i=1; i <=n; i++){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print("  ");
+                }else
+                {
+                    System.out.print(" " + i);
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+        
+        //Equilateral Triangle
+        System.out.println("Equilateral Triangle");
+        for(int i=n; i >0; i--){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print(" ");
+                }else
+                {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+        
+        //Equilateral Triangle Inverse
+        System.out.println("Equilateral Triangle Inverse");
+        for(int i=1; i <=n; i++){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print(" ");
+                }else
+                {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+
+        //Right Angular Triangle Align Left Outline
+        System.out.println("Right Angular Triangle Align Left Outline");
+        for(int i=1; i <= n; i++){
+            for(int j=1; j<=i; j++){
+                if(j==i || j==1 || i==n){
+                    System.out.print(i + " ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+
+        //Right Angular Triangle Align Left-Up Outline
+        System.out.println("Right Angular Triangle Align Left-Up Outline");
+        for(int i=n; i >0; i--){
+            for(int j=1; j<=i; j++){
+                if(j==i || j==1 || i==n){
+                    System.out.print(i + " ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+
+        //Right Angular Triangle Align Right Outline
+        System.out.println("Right Angular Triangle Align Right Outline");
+        for(int i=n; i >0; i--){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print("  ");
+                }else if(j==i || j==n || i==1){
+                    System.out.print(i +" ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+
+        //Right Angular Triangle Align Up-Right Outline
+        System.out.println("Right Angular Triangle Align Up-Right Outline");
+        for(int i=1; i <=n; i++){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print("  ");
+                }else if(j==i || j==n || i==1){
+                    System.out.print(i + " ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+        
+        //Equilateral Triangle Outline
+        System.out.println("Equilateral Triangle Outline");
+        for(int i=n; i >0; i--){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print(" ");
+                }else if(j==i || j==n || i==1){
+                    System.out.print(i+ " ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+        
+        //Equilateral Triangle Inverse Outline
+        System.out.println("Equilateral Triangle Inverse Outline");
+        for(int i=1; i <=n; i++){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print(" ");
+                }else if(j==i || j==n || i==1){
+                    System.out.print(i + " ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+        
+        //Diamond Outline
+        System.out.println("Diamond Outline");
+        for(int i=n; i >1; i--){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print(" ");
+                }else if(j==i || j==n){
+                    System.out.print(i+ " ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+        for(int i=1; i <=n; i++){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print(" ");
+                }else if(j==i || j==n ){
+                    System.out.print(i +" ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+        
+        //Diamond
+        System.out.println("Diamond");
+        for(int i=n; i >1; i--){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print(" ");
+                }else
+                {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println(" ");
+        }
+        for(int i=1; i <=n; i++){
+            for(int j=1; j<=n; j++){
+                if(j<i){
+                    System.out.print(" ");
+                }else
+                {
+                    System.out.print(i+" ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+    }
+}
